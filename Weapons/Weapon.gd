5a8cf3ac0e4ecb03f9fc331bdd4e8f3ab865cc3b
SpaceyLake <niklas_hornblad@hotmail.com>
2022-04-02 18:35:14 +0200

    Fixed weapon merge

--- Weapons/Weapon.gd
+++ Weapons/Weapon.gd
@@ -6,14 +6,10 @@ onready var _cooldown_timer : Timer = $CooldownTimer
 onready var _mussle : Node2D = $Mussle
 
 func _fire(direction : Vector2):
-<<<<<<< HEAD
 	if _cooldown_timer.time_left <= 0:
 		_cooldown_timer.start(_cooldown_time)
 		var _bullet_instance = Global.instance_node(_bullet, _mussle.global_position, Global._node_creation_parent)
 		_bullet_instance._fire(direction)
-=======
-	var bullet_instance = Global.instance_node(bullet, mussle.global_position, Global._node_creation_parent)
-	bullet_instance._fire(direction)
 
 func _set_animation(idle : bool, dir : Vector2):
 	if dir.y < 0:
@@ -34,4 +30,3 @@ func _set_animation(idle : bool, dir : Vector2):
 
 func _set_right(right : bool):
 	scale.x = (2*int(right))-1
->>>>>>> c19b8b29899bd101de801027dc677af452ef3968
