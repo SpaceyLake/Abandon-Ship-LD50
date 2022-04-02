6691f134cce44e9aa623d8e9b9d4c81670703b07
SpaceyLake <niklas_hornblad@hotmail.com>
2022-04-02 18:20:28 +0200

    Fixed knockback + enemy snawning

--- Enemy/Enemy.gd
+++ Enemy/Enemy.gd
@@ -1,5 +1,7 @@
 extends KinematicBody2D
 
+signal _enemy_killed()
+
 export var _velocity : Vector2 = Vector2.ZERO
 var _input : Vector2 = Vector2.ZERO
 export var _velocity_decrease : float = 0.85
@@ -9,31 +11,33 @@ export var _minimum_velocity : float = 0.01
 export var _health : int = 5
 export var _damage : int = 1
 export var _knockback = 100
-var _known_player_position : Vector2 = Vector2.ZERO
+var _target_position : Vector2 = Vector2.ZERO
 onready var _vision : RayCast2D = $Vision
 onready var _attack_area : Area2D = $AttackArea
-
 
 var _is_attacking : bool = false
 
 func _ready():
 	add_to_group("Enemy", true)
-	_attack_area.connect("body_entered", Global._player, "_attacked", [_damage, _knockback, position])
-	_known_player_position = position
+	_attack_area.connect("body_entered", self, "_attacking")
+	_target_position = global_position
+
+func _set_target_position(target_position):
+	_target_position = target_position
 
 func _physics_process(delta):
 	#_vision.transform.
 	if Global._player != null and !_is_attacking:
 		_vision.look_at(Global._player.transform.origin)
 		if _vision.get_collider() == Global._player:
-			_known_player_position = _vision.get_collision_point()
-			if transform.origin.distance_to(_known_player_position) < _attack_distance:
+			_target_position = _vision.get_collision_point()
+			if transform.origin.distance_to(_target_position) < _attack_distance:
 				_attack_start()
 			$Vision/Icon.modulate = Color.green
 		else:
 			$Vision/Icon.modulate = Color.blue
-		if global_position.distance_to(_known_player_position) > 4:
-			_input = (_known_player_position - global_position).normalized()
+		if global_position.distance_to(_target_position) > 4:
+			_input = (_target_position - global_position).normalized()
 		else:
 			_input = Vector2.ZERO
 		_velocity += _input * _movement_speed * delta
@@ -53,13 +57,13 @@ func _physics_process(delta):
 				$Sprite.flip_h = _input.x < 0
 
 func _bullet_hit(damage, knockback, bullet_velocity, _bullet_origin):
-	_known_player_position = _bullet_origin
+	_target_position = _bullet_origin
 	_health -= damage
 	_velocity += bullet_velocity * knockback
 	print(_velocity)
 	if _health <= 0:
+		emit_signal("_enemy_killed")
 		queue_free()
-		_attack_area.disconnect("body_entered", Global._player, "_attacked")
 
 func _attack_start():
 	_is_attacking = true
@@ -73,3 +77,6 @@ func _attack_end_trigger():
 
 func _attack_finished():
 	_is_attacking = false
+
+func _attacking(_body):
+	Global._player._attacked(_damage, _knockback * (Global._player.global_position - global_position).normalized());
--- Player/Player.gd
+++ Player/Player.gd
@@ -48,8 +48,8 @@ func _physics_process(delta):
 func _exit_tree():
 	Global._player = null
 
-func _attacked(_body, damage, _knockback, enemy_position):
-	print(enemy_position)
+func _attacked(damage, _knockback):
+	_velocity += _knockback
 	_health -= damage
 	emit_signal("health_changed", _health)
 	if _health <= 0: get_tree().reload_current_scene()
