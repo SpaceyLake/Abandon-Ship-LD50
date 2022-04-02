4a363cde30584cba44c012362bbc8be1ada544d0
SpaceyLake <niklas_hornblad@hotmail.com>
2022-04-02 22:25:18 +0200

    Added and updated UI elements(bars)

--- UI/UI.gd
+++ UI/UI.gd
@@ -1,6 +1,9 @@
 extends CanvasLayer
 
-onready var _healthbar = $Healthbar
+onready var _healthbar = $Healthbar/Healthbar
+onready var _oxygenbar = $Oxygenbar/Oxygenbar
+onready var _hullbar = $Hullbar/Hullbar
+onready var _evacuationbar = $Evacuationbar/Evacuationbar
 
 func _ready():
 	Global._UI = self
@@ -8,8 +11,26 @@ func _ready():
 func _exit_tree():
 	Global._UI = null
 
-func _update_max_health(max_health):
+func _set_max_health(max_health):
 	_healthbar.max_value = max_health
 
-func _update_healthbar(health):
+func _set_healthbar(health):
 	_healthbar.value = health
+
+func _set_max_oxygen(max_oxygen):
+	_oxygenbar.max_value = max_oxygen
+	
+func _set_oxygen_level(oxygen_level):
+	_oxygenbar.value = oxygen_level
+
+func _set_max_hull(max_hull):
+	_hullbar.max_value = max_hull
+
+func _set_hull_integrity(hull_integrity):
+	_hullbar.value = hull_integrity
+
+func _set_total_crew(total_crew):
+	_evacuationbar.max_value = total_crew
+
+func _set_evacuated_crew(evacuated_crew):
+	_evacuationbar.value = evacuated_crew
--- World.gd
+++ World.gd
@@ -1,8 +1,36 @@
 extends Node2D
 
+export var _max_hull_integrity : int = 100
+export var _max_oxygen_level : int = 100
+export var _total_crew : int = 100
+export var _evacuation_time : float = 2
+var _hull_integrity : int
+var _oxygen_level : int
+var _evacuated_crew : int
+onready var _evacuation_timer : Timer = $EvacuationTimer
 
 func _ready():
 	Global._node_creation_parent = self
+	_hull_integrity = _max_hull_integrity
+	_oxygen_level = _max_oxygen_level
+	_evacuated_crew = 0
+	Global._UI._set_max_oxygen(_max_oxygen_level)
+	Global._UI._set_oxygen_level(_oxygen_level)
+	Global._UI._set_max_hull(_max_hull_integrity)
+	Global._UI._set_hull_integrity(_hull_integrity)
+	Global._UI._set_total_crew(_total_crew)
+	Global._UI._set_evacuated_crew(_evacuated_crew)
+	_evacuation_timer.connect("timeout", self, "_evacuate")
+	_evacuation_timer.start(_evacuation_time)
+
+func _damage_hull():
+	_hull_integrity -= 1
+	Global._UI._set_hull_integrity(_hull_integrity)
+
+func _evacuate():
+	_evacuated_crew += 1
+	Global._UI._set_evacuated_crew(_evacuated_crew)
+	_evacuation_timer.start(_evacuation_time)
 
 func _exit_tree():
 	Global._node_creation_parent = null
