9f56b4315682c08343b6238cab20a5214e390fe1
SpaceyLake <niklas_hornblad@hotmail.com>
2022-04-02 17:02:58 +0200

    Bullet update

--- Weapons/Bullet.gd
+++ Weapons/Bullet.gd
@@ -1,20 +1,24 @@
 extends Area2D
 
 var _velocity : Vector2 = Vector2.ZERO
+var _direction : Vector2 = Vector2.ZERO
 export var _speed : float = 100
-export var _knockback : float = 100
+export var _knockback : float = 2
 export var _damage : float = 1
+var _spawn_position : Vector2 = Vector2.ZERO
 
 func _ready():
 	connect("body_entered", self, "_collide")
 
 func _fire(direction : Vector2):
 	_velocity = direction * _speed
+	_direction = direction
+	_spawn_position = global_position
 
 func _physics_process(delta):
 	position += _velocity * delta
 
 func _collide(body):
 	if body.is_in_group("Enemy"):
-		body._bullet_hit(_damage, _knockback, transform.origin)
+		body._bullet_hit(_damage, _knockback, _direction, _spawn_position)
 	queue_free()
