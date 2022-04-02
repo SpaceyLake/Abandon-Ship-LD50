f11c6669d2441663c735e5eb4eafa3eeab656f0b
Golahirom <golahirom@gmail.com>
2022-04-02 12:16:00 +0200

    Added animation to player

--- Player/Player.gd
+++ Player/Player.gd
@@ -1,5 +1,6 @@
 extends KinematicBody2D
 
+var _input : Vector2 = Vector2.ZERO
 var _velocity : Vector2 = Vector2.ZERO
 export var _velocity_decrease = 0.9
 export var _movement_speed : int = 100
@@ -9,9 +10,18 @@ func _ready():
 	pass
 
 func _physics_process(delta):
-	_velocity += Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-						Input.get_action_strength("move_down") - Input.get_action_strength("move_up")).normalized() * delta * _movement_speed
+	_input = Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+						Input.get_action_strength("move_down") - Input.get_action_strength("move_up")).normalized()
+	_velocity += _input * delta * _movement_speed
 	_velocity *= _velocity_decrease
 	if abs(_velocity.x) < _minimum_velocity: _velocity = Vector2(0, _velocity.y)
 	if abs(_velocity.y) < _minimum_velocity: _velocity = Vector2(_velocity.x, 0)
 	_velocity = move_and_slide(_velocity, Vector2.ZERO, false)
+	
+	#Animation
+	if _input == Vector2.ZERO:
+		$AnimationPlayer.play("Idle")
+	else:
+		$AnimationPlayer.play("Walk")
+		if _input.x != 0:
+			$Sprite.flip_h = _input.x < 0
