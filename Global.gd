5de0a59d0afd5c5a508b48e2f5068e769d4ab12f
SpaceyLake <niklas_hornblad@hotmail.com>
2022-04-02 14:54:04 +0200

    Node creation

--- Global.gd
+++ Global.gd
@@ -1,3 +1,12 @@
 extends Node
 
 var _player : KinematicBody2D = null
+
+var _node_creation_parent = null
+
+func instance_node(node, location, parent):
+	if parent == null: return null
+	var node_instance = node.instance()
+	parent.add_child(node_instance)
+	node_instance.global_position = location
+	return node_instance
