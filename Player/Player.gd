6871d71f632f7a9a3dd628b21410430e653c6a96
SpaceyLake <niklas_hornblad@hotmail.com>
2022-04-02 11:26:00 +0200

    Started on a player

---
+++ Player/Player.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+var _velocity : Vector2 = Vector2.ZERO
+export var _velocity_decrease = 0.9
+export var _movement_speed : int = 100
+export var _minimum_velocity : float = 0.01
+
+func _ready():
+	pass
+
+func _physics_process(delta):
+	_velocity += Vector2(Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
+						Input.get_action_strength("move_down") - Input.get_action_strength("move_up")).normalized() * delta * _movement_speed
+	_velocity *= _velocity_decrease
+	if abs(_velocity.x) < _minimum_velocity: _velocity = Vector2(0, _velocity.y)
+	if abs(_velocity.y) < _minimum_velocity: _velocity = Vector2(_velocity.x, 0)
+	_velocity = move_and_slide(_velocity, Vector2.ZERO, false)
