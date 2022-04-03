c6527c348d21e19f873aac06b10dfd79c80754ef
SpaceyLake <niklas_hornblad@hotmail.com>
2022-04-03 15:34:29 +0200

    Added escape pod control

---
+++ EscapePodControl/EscapePodControl.gd
@@ -0,0 +1,13 @@
+extends StaticBody2D
+
+signal _win()
+
+export(NodePath) var _door_path
+onready var _door = get_node(_door_path)
+
+func _interact():
+	_door.close()
+	emit_signal("_win")
+
+func _stop_interaction():
+	pass
