fabf051d0557d46b29330d367321869fec408daa
SpaceyLake <niklas_hornblad@hotmail.com>
2022-04-03 23:50:17 +0200

    Extended wait time

--- End.gd
+++ End.gd
@@ -1,6 +1,6 @@
 extends Control
 
-export var _wait_time : float = 0.5
+export var _wait_time : float = 1
 onready var _wait_timer : Timer = $WaitTimer
 var _ready = false
 
@@ -11,7 +11,7 @@ func _ready():
 	_wait_timer.start()
 
 func _process(delta):
-	if Input.is_action_just_pressed("fire_down") or Input.is_action_just_pressed("fire_left") or Input.is_action_just_pressed("fire_right") or Input.is_action_just_pressed("fire_up") or Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("move_down") or Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("move_right") or Input.is_action_just_pressed("move_up") and _ready:
+	if Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("escape") and _ready:
 		get_tree().change_scene("res://MainMenu.tscn")
 
 func _set_ready():
