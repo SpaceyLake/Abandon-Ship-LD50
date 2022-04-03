bd1cf77dd1653b4ca5694fa995a744f48061cbb9
SpaceyLake <niklas_hornblad@hotmail.com>
2022-04-03 21:18:42 +0200

    Avoiding missing end screen with timer

--- End.gd
+++ End.gd
@@ -1,5 +1,18 @@
 extends Control
 
+export var _wait_time : float = 0.5
+onready var _wait_timer : Timer = $WaitTimer
+var _ready = false
+
+func _ready():
+	_wait_timer.wait_time = _wait_time
+	_wait_timer.one_shot = true
+	_wait_timer.connect("timeout", self, "_set_ready")
+	_wait_timer.start()
+
 func _process(delta):
-	if Input.is_action_just_pressed("fire_down") or Input.is_action_just_pressed("fire_left") or Input.is_action_just_pressed("fire_right") or Input.is_action_just_pressed("fire_up") or Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("move_down") or Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("move_right") or Input.is_action_just_pressed("move_up"):
+	if Input.is_action_just_pressed("fire_down") or Input.is_action_just_pressed("fire_left") or Input.is_action_just_pressed("fire_right") or Input.is_action_just_pressed("fire_up") or Input.is_action_just_pressed("interact") or Input.is_action_just_pressed("move_down") or Input.is_action_just_pressed("move_left") or Input.is_action_just_pressed("move_right") or Input.is_action_just_pressed("move_up") and _ready:
 		get_tree().change_scene("res://MainMenu.tscn")
+
+func _set_ready():
+	_ready = true
