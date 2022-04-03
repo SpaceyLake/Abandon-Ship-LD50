ae2cc6dfa74d38f7417130d4fc2f8812d155c9a4
SpaceyLake <niklas_hornblad@hotmail.com>
2022-04-03 18:45:44 +0200

    Difficulty tweeks

--- EscapePodControl/EscapePodControl.gd
+++ EscapePodControl/EscapePodControl.gd
@@ -5,6 +5,9 @@ signal _win()
 export(NodePath) var _door_path
 onready var _door = get_node(_door_path)
 
+func _ready():
+	add_to_group("EscapePodControl")
+
 func _interact():
 	_door.close()
 	emit_signal("_win")
